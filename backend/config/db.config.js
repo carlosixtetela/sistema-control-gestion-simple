6c89a3799d0d01afec8808d98efca2919997caee
Carlos Ixtetelá <41091085+carlosIxte@users.noreply.github.com>
2024-10-27 08:34:47 -0600

    fix

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sistema_gestion_pruebas', 'pruebas_user', 'Cidl2024_..', {
-  host: 'localhost',
+const sequelize = new Sequelize('bx5ikdtwgrhibpgi99jk', 'udqzb0jo8kselm2w', 'JxSVsHFQNzz7tyWZ8ING', {
+  host: 'bx5ikdtwgrhibpgi99jk-mysql.services.clever-cloud.com',
   dialect: 'mysql',
 });
 
